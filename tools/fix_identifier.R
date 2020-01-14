896f9c292561d8a1855481437a27ab283a988c08
GreysonL <gang_liu@g.harvard.edu>
2020-01-14 16:57:09 -0500

    add a small function

---
+++ tools/fix_identifier.R
@@ -0,0 +1,11 @@
+ids = list.files("F:/DATA/hope")
+for(id in ids){
+  name = list.files(paste0("F:/DATA/hope/",id,"/identifiers"))[1]
+  file = read.csv(paste0("F:/DATA/hope/",id,"/identifiers/",name),header=T)
+  temp = as.matrix(file)[1,]
+  temp = c(row.names(file)[1],as.vector(temp[-13]))
+  row.names(file) = NULL
+  temp = as.data.frame(t(temp))
+  colnames(temp) = colnames(file)
+  write.csv(temp,paste0("F:/DATA/hope/",id,"/identifiers/",name),row.names = F)
+}
